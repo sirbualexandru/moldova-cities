4c9ffa3f74df385a4788b9a3e42b44f7b5a17e3d
Alexandru Sîrbu <sirbu.alexandru.andrei@gmail.com>
2017-11-25 21:34:10 +0200

    Upload cities SQL file

---
+++ cities.sql
@@ -0,0 +1,51 @@
+#drop table cities;
+
+create table cities
+(
+  id integer primary key,
+  name varchar (30),
+);
+
+insert into cities (id, name) values (1, 'Chisinau');
+insert into cities (id, name) values (38, 'Anenii Noi');
+insert into cities (id, name) values (2, 'Balti');
+insert into cities (id, name) values (43, 'Basarabeasca');
+insert into cities (id, name) values (33, 'Bender (Tighina)');
+insert into cities (id, name) values (29, 'Briceni');
+insert into cities (id, name) values (4, 'Cahul');
+insert into cities (id, name) values (32, 'Calarasi');
+insert into cities (id, name) values (9, 'Cantemir');
+insert into cities (id, name) values (10, 'Causeni');
+insert into cities (id, name) values (22, 'Ceadir-Lunga');
+insert into cities (id, name) values (23, 'Cimislia');
+insert into cities (id, name) values (11, 'Comrat');
+insert into cities (id, name) values (12, 'Cricova');
+insert into cities (id, name) values (40, 'Criuleni');
+insert into cities (id, name) values (30, 'Donduseni');
+insert into cities (id, name) values (6, 'Drochia');
+insert into cities (id, name) values (36, 'Dubasari');
+insert into cities (id, name) values (7, 'Edinet');
+insert into cities (id, name) values (25, 'Falesti');
+insert into cities (id, name) values (26, 'Floresti');
+insert into cities (id, name) values (5, 'Glodeni');
+insert into cities (id, name) values (27, 'Hincesti');
+insert into cities (id, name) values (28, 'Ialoveni');
+insert into cities (id, name) values (13, 'Leova');
+insert into cities (id, name) values (14, 'Leuseni');
+insert into cities (id, name) values (15, 'Nisporeni');
+insert into cities (id, name) values (31, 'Ocnita');
+insert into cities (id, name) values (16, 'Orhei');
+insert into cities (id, name) values (17, 'Rezina');
+insert into cities (id, name) values (35, 'Ribnita');
+insert into cities (id, name) values (18, 'Riscani');
+insert into cities (id, name) values (39, 'Singerei');
+insert into cities (id, name) values (42, 'Soldanesti');
+insert into cities (id, name) values (19, 'Soroca');
+insert into cities (id, name) values (8, 'Stefan Voda');
+insert into cities (id, name) values (41, 'Straseni');
+insert into cities (id, name) values (20, 'Taraclia');
+insert into cities (id, name) values (21, 'Telenesti');
+insert into cities (id, name) values (34, 'Tiraspol');
+insert into cities (id, name) values (24, 'Ungheni');
+insert into cities (id, name) values (37, 'Vadul lui Voda');
+insert into cities (id, name) values (3, 'Vulcanesti');
